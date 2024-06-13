06f5b088c735573dfc2a27d873ab993d3d45afba
Racheli <viderracheli@gamil.com>
2024-06-13 16:13:00 +0300

    only the doch needs uploud

--- שלב ב/Constraints.sql
+++ שלב ב/Constraints.sql
@@ -1,6 +1,6 @@
 -- Movies Table
 ALTER TABLE Movies
-MODIFY Duration INT NOT NULL CHECK (Duration <= 180);
+MODIFY Duration INT CHECK (Duration <= 180);
 
 ALTER TABLE Movies
 MODIFY Rating INT CHECK (Rating >= 1 AND Rating <= 10);
@@ -11,22 +11,22 @@ ADD CONSTRAINT unique_hour_day_date UNIQUE(HourH, DayD, DateD);
 
 -- Theaters Table
 ALTER TABLE Theaters
-MODIFY Capacity INT NOT NULL CHECK (Capacity >= 0);
+MODIFY Capacity INT  CHECK (Capacity >= 0);
 
 -- Schedules Table
 ALTER TABLE Schedules
-MODIFY AvailableSeats INT NOT NULL DEFAULT 0 CHECK (AvailableSeats >= 0);
+MODIFY AvailableSeats INT DEFAULT 0 CHECK (AvailableSeats >= 0);
 
 ALTER TABLE Schedules
 ADD CONSTRAINT unique_date_theater UNIQUE(DateID, TheaterID);
 
 -- Subscribers Table
 ALTER TABLE Subscribers
-MODIFY DiscountPercents INT NOT NULL CHECK(DiscountPercents IN (10, 20, 30, 40, 50));
+MODIFY DiscountPercents INT DEFAULT 30 CHECK(DiscountPercents IN (10, 20, 30, 40, 50));
 
 -- TicketSales Table
 ALTER TABLE TicketSales
-MODIFY Price NUMERIC(5, 2) NOT NULL DEFAULT 30 CHECK(Price IN (30, 50, 90));
+MODIFY Price NUMERIC(5, 2) CHECK(Price IN (30, 50, 90));
 
 ALTER TABLE TicketSales
 MODIFY SubscriberID NUMERIC(3) DEFAULT NULL;
